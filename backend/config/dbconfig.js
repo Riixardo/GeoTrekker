6cb41c068f48ee1bfea99b2f6637933de788a9bc
Richard Chen <RicardoMonriguez2@gmail.com>
2024-07-29 14:37:00 -0700

    Create dbconfig.js

---
+++ backend/config/dbconfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    postgres: {
+        user: 'INSERT USER HERE',
+        host: 'INSERT HOST HERE',
+        database: 'INSERT DB NAME HERE',
+        password: 'INSERT PASSWORD HERE',
+        port: INSERT PORT NUMBER HERE,
+    }
+};
